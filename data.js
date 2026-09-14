0a814f673673f7be02643a530d0fa53f8da3bfeb
AS-ITC <ibiizeries@gmail.com>
2026-09-14 08:32:24 +0700

    Add files via upload

---
+++ data.js
@@ -0,0 +1,763 @@
+// data.js — รายชื่อผู้ตรวจ + พื้นที่/โซน ("master data")
+// ไฟล์นี้แก้ไขโดย Dev เท่านั้น ผ่าน GitHub (เปิดไฟล์นี้ในเว็บ GitHub -> แก้ไข -> Commit)
+// ทุกคนที่เปิดแอปจะเห็นข้อมูลชุดเดียวกันเสมอ ไม่ต้องตั้งค่าอะไรเพิ่มนอกจาก GitHub ที่มีอยู่แล้ว
+window.MASTER_DATA = {
+  "members": [
+    {
+      "id": "m22",
+      "name": "กัลยา ผลบุญ",
+      "department": "PF1"
+    },
+    {
+      "id": "m40",
+      "name": "จันทร์เพ็ญ รุ่งเรือง",
+      "department": "WH"
+    },
+    {
+      "id": "m25",
+      "name": "จิราภรณ์ จันทร์แจ่ม",
+      "department": "AC"
+    },
+    {
+      "id": "m19",
+      "name": "ชัยวัฒน์ สายทอง",
+      "department": "EN"
+    },
+    {
+      "id": "m24",
+      "name": "ชาญชัย ทิพย์รัตน์",
+      "department": "WH"
+    },
+    {
+      "id": "m16",
+      "name": "ชาญชัย บัวขาว",
+      "department": "WH"
+    },
+    {
+      "id": "m41",
+      "name": "ชาญชัย แก้วมณี",
+      "department": "AC"
+    },
+    {
+      "id": "m32",
+      "name": "ดวงใจ รุ่งเรือง",
+      "department": "WH"
+    },
+    {
+      "id": "m11",
+      "name": "ธนา ทองดี",
+      "department": "EN"
+    },
+    {
+      "id": "m38",
+      "name": "ธิดา อินทร์แก้ว",
+      "department": "PF1"
+    },
+    {
+      "id": "m36",
+      "name": "นงลักษณ์ จันทร์แจ่ม",
+      "department": "HR"
+    },
+    {
+      "id": "m21",
+      "name": "นงลักษณ์ วงศ์ษา",
+      "department": "PE"
+    },
+    {
+      "id": "m23",
+      "name": "นงลักษณ์ แก้วมณี",
+      "department": "PF2"
+    },
+    {
+      "id": "m47",
+      "name": "นันทนา บุญมี",
+      "department": "PF2"
+    },
+    {
+      "id": "m1",
+      "name": "นันทนา ใจดี",
+      "department": "AC"
+    },
+    {
+      "id": "m43",
+      "name": "ประเสริฐ มั่นคง",
+      "department": "EN"
+    },
+    {
+      "id": "m14",
+      "name": "ประเสริฐ สายทอง",
+      "department": "PF1"
+    },
+    {
+      "id": "m6",
+      "name": "ปราณี โพธิ์ทอง",
+      "department": "PF1"
+    },
+    {
+      "id": "m9",
+      "name": "พรทิพย์ ศรีสุข",
+      "department": "AC"
+    },
+    {
+      "id": "m44",
+      "name": "พรทิพย์ เพชรรัตน์",
+      "department": "HR"
+    },
+    {
+      "id": "m5",
+      "name": "รัตนา รุ่งเรือง",
+      "department": "PE"
+    },
+    {
+      "id": "m42",
+      "name": "รัตนา สว่างวงศ์",
+      "department": "CS"
+    },
+    {
+      "id": "m30",
+      "name": "วรวุฒิ รุ่งเรือง",
+      "department": "PF1"
+    },
+    {
+      "id": "m10",
+      "name": "วรวุฒิ โพธิ์ทอง",
+      "department": "CS"
+    },
+    {
+      "id": "m46",
+      "name": "วิชัย วิริยะกุล",
+      "department": "PF1"
+    },
+    {
+      "id": "m17",
+      "name": "วิภา แก้วมณี",
+      "department": "AC"
+    },
+    {
+      "id": "m35",
+      "name": "วีระ รักงาน",
+      "department": "EN"
+    },
+    {
+      "id": "m48",
+      "name": "วีระ หอมจันทร์",
+      "department": "WH"
+    },
+    {
+      "id": "m18",
+      "name": "ศิริพร แก้วมณี",
+      "department": "CS"
+    },
+    {
+      "id": "m39",
+      "name": "สมบัติ สว่างวงศ์",
+      "department": "PF2"
+    },
+    {
+      "id": "m12",
+      "name": "สมศักดิ์ ทองดี",
+      "department": "HR"
+    },
+    {
+      "id": "m37",
+      "name": "สมศักดิ์ ผลบุญ",
+      "department": "PE"
+    },
+    {
+      "id": "m29",
+      "name": "สมศักดิ์ พูลสวัสดิ์",
+      "department": "PE"
+    },
+    {
+      "id": "m28",
+      "name": "สมหญิง รุ่งเรือง",
+      "department": "HR"
+    },
+    {
+      "id": "m45",
+      "name": "สายฝน จันทร์แจ่ม",
+      "department": "PE"
+    },
+    {
+      "id": "m33",
+      "name": "สิทธิชัย เพชรรัตน์",
+      "department": "AC"
+    },
+    {
+      "id": "m4",
+      "name": "สุภาพร อินทร์แก้ว",
+      "department": "HR"
+    },
+    {
+      "id": "m26",
+      "name": "สุเมธ รักงาน",
+      "department": "CS"
+    },
+    {
+      "id": "m27",
+      "name": "อภิสิทธิ์ เพชรรัตน์",
+      "department": "EN"
+    },
+    {
+      "id": "m7",
+      "name": "อรุณี ทองดี",
+      "department": "PF2"
+    },
+    {
+      "id": "m13",
+      "name": "อรุณี รุ่งเรือง",
+      "department": "PE"
+    },
+    {
+      "id": "m31",
+      "name": "อรุณี หอมจันทร์",
+      "department": "PF2"
+    },
+    {
+      "id": "m2",
+      "name": "อัมพร ชูเกียรติ",
+      "department": "CS"
+    },
+    {
+      "id": "m49",
+      "name": "อัมพร อินทร์แก้ว",
+      "department": "AC"
+    },
+    {
+      "id": "m34",
+      "name": "เพ็ญศรี บุญมี",
+      "department": "CS"
+    },
+    {
+      "id": "m3",
+      "name": "เพ็ญศรี รุ่งเรือง",
+      "department": "EN"
+    },
+    {
+      "id": "m15",
+      "name": "เพ็ญศรี สุขสันต์",
+      "department": "PF2"
+    },
+    {
+      "id": "m8",
+      "name": "เอกชัย ทองดี",
+      "department": "WH"
+    },
+    {
+      "id": "m50",
+      "name": "เอกชัย วงศ์ษา",
+      "department": "CS"
+    },
+    {
+      "id": "m20",
+      "name": "ไพศาล บุญมี",
+      "department": "HR"
+    }
+  ],
+  "areas": [
+    {
+      "id": "a1",
+      "location": "AC",
+      "zone": "office ชั้น 3-office AC,FI,ห้องน้ำ",
+      "order": 0
+    },
+    {
+      "id": "a2",
+      "location": "CS",
+      "zone": "ห้องเย็น3- office",
+      "order": 0
+    },
+    {
+      "id": "a3",
+      "location": "EN",
+      "zone": "office บ่อน้ำเสีย",
+      "order": 0
+    },
+    {
+      "id": "a4",
+      "location": "HR",
+      "zone": "โรงอาหาร3",
+      "order": 0
+    },
+    {
+      "id": "a5",
+      "location": "PE",
+      "zone": "Workshop PE/PF1-2",
+      "order": 0
+    },
+    {
+      "id": "a6",
+      "location": "PF1",
+      "zone": "โรงสกัดน้ำนั่งไก่/ห้องต้มข้าว",
+      "order": 0
+    },
+    {
+      "id": "a7",
+      "location": "PF2",
+      "zone": "เตรียม chunk,loaf",
+      "order": 0
+    },
+    {
+      "id": "a8",
+      "location": "WH",
+      "zone": "ป้อม--3",
+      "order": 0
+    },
+    {
+      "id": "a9",
+      "location": "CS",
+      "zone": "ห้องเย็น3-เตรียมผัก",
+      "order": 1
+    },
+    {
+      "id": "a10",
+      "location": "CS",
+      "zone": "ห้องเย็น3 -ลานรับวัตถุดิบ",
+      "order": 2
+    },
+    {
+      "id": "a11",
+      "location": "CS",
+      "zone": "ห้องเย็น3 -ห้องเก็บcold-storage",
+      "order": 3
+    },
+    {
+      "id": "a12",
+      "location": "CS",
+      "zone": "ห้องเย็น3 -ห้องเครื่องทำความเย็น3",
+      "order": 4
+    },
+    {
+      "id": "a13",
+      "location": "CS",
+      "zone": "ห้องเย็น3 -ห้องแกะปลา",
+      "order": 5
+    },
+    {
+      "id": "a14",
+      "location": "CS",
+      "zone": "ห้องเย็น3 -ห้องBonepowder",
+      "order": 6
+    },
+    {
+      "id": "a15",
+      "location": "CS",
+      "zone": "ห้องเย็น3 -ห้องfish Extract",
+      "order": 7
+    },
+    {
+      "id": "a16",
+      "location": "CS",
+      "zone": "ห้องเย็น4",
+      "order": 8
+    },
+    {
+      "id": "a17",
+      "location": "CS",
+      "zone": "อาคารPilot plant",
+      "order": 9
+    },
+    {
+      "id": "a18",
+      "location": "CS",
+      "zone": "ห้องเย็น2",
+      "order": 10
+    },
+    {
+      "id": "a19",
+      "location": "CS",
+      "zone": "ห้องละลายไฟฟ้า",
+      "order": 11
+    },
+    {
+      "id": "a20",
+      "location": "CS",
+      "zone": "ห้องตาชั่ง",
+      "order": 12
+    },
+    {
+      "id": "a21",
+      "location": "EN",
+      "zone": "Workshop ช่าง -จุดคัดแยกขยะ",
+      "order": 1
+    },
+    {
+      "id": "a22",
+      "location": "EN",
+      "zone": "บ่อน้ำดี",
+      "order": 2
+    },
+    {
+      "id": "a23",
+      "location": "EN",
+      "zone": "workshop PE/EN บอยเลอร์ ซ่อมบำรุง ช่างไฟ ช่างก่อสร้าง",
+      "order": 3
+    },
+    {
+      "id": "a24",
+      "location": "EN",
+      "zone": "OfficePE/EN",
+      "order": 4
+    },
+    {
+      "id": "a25",
+      "location": "EN",
+      "zone": "อาคารบอยเลอร์ชีวมวล",
+      "order": 5
+    },
+    {
+      "id": "a26",
+      "location": "HR",
+      "zone": "ป้อม--7",
+      "order": 1
+    },
+    {
+      "id": "a27",
+      "location": "HR",
+      "zone": "ห้องซักอบรีด",
+      "order": 2
+    },
+    {
+      "id": "a28",
+      "location": "HR",
+      "zone": "โรงอาหาร2",
+      "order": 3
+    },
+    {
+      "id": "a29",
+      "location": "HR",
+      "zone": "โรงอาหาร1-ป้อม1 ป้อม2 ,SHE",
+      "order": 4
+    },
+    {
+      "id": "a30",
+      "location": "HR",
+      "zone": "โรงอาหาร1-ห้องน้ำ,ห้องละหมาด",
+      "order": 5
+    },
+    {
+      "id": "a31",
+      "location": "HR",
+      "zone": "ห้องฝากสัมภาระ",
+      "order": 6
+    },
+    {
+      "id": "a32",
+      "location": "HR",
+      "zone": "office ชั้น 1-สำนักงาน HR /ห้องประชุม/ห้องน้ำชั้น 1/ห้องพยาบาล01",
+      "order": 7
+    },
+    {
+      "id": "a33",
+      "location": "HR",
+      "zone": "office ชั้น 2-officeฝ่าย PN,PF1,QC,IT,PO,BQ,ห้องอบรม1-3,ห้องน้ำ",
+      "order": 8
+    },
+    {
+      "id": "a34",
+      "location": "HR",
+      "zone": "ห้องพยาบาล02",
+      "order": 9
+    },
+    {
+      "id": "a35",
+      "location": "PE",
+      "zone": "Overhual",
+      "order": 1
+    },
+    {
+      "id": "a36",
+      "location": "PF1",
+      "zone": "ห้อง sachet",
+      "order": 1
+    },
+    {
+      "id": "a37",
+      "location": "PF1",
+      "zone": "ห้อง Pouch",
+      "order": 2
+    },
+    {
+      "id": "a38",
+      "location": "PF1",
+      "zone": "ห้องเตรียมผัก/ห้องเย็น",
+      "order": 3
+    },
+    {
+      "id": "a39",
+      "location": "PF1",
+      "zone": "ห้อง Retort Store (VS เก่า)",
+      "order": 4
+    },
+    {
+      "id": "a40",
+      "location": "PF1",
+      "zone": "ห้องขูดเลือด",
+      "order": 5
+    },
+    {
+      "id": "a41",
+      "location": "PF1",
+      "zone": "บรรจุ CUP",
+      "order": 6
+    },
+    {
+      "id": "a42",
+      "location": "PF1",
+      "zone": "บรรจุ CAN",
+      "order": 7
+    },
+    {
+      "id": "a43",
+      "location": "PF1",
+      "zone": "Store/Ink",
+      "order": 8
+    },
+    {
+      "id": "a44",
+      "location": "PF1",
+      "zone": "หม้ออบปลา",
+      "order": 9
+    },
+    {
+      "id": "a45",
+      "location": "PF1",
+      "zone": "จุดเตรียม chunk",
+      "order": 10
+    },
+    {
+      "id": "a46",
+      "location": "PF1",
+      "zone": "ห้องเย็น PF1",
+      "order": 11
+    },
+    {
+      "id": "a47",
+      "location": "PF1",
+      "zone": "ห้องเครื่องทำความเย็น PF1",
+      "order": 12
+    },
+    {
+      "id": "a48",
+      "location": "PF1",
+      "zone": "ไลน์CUP1-4",
+      "order": 13
+    },
+    {
+      "id": "a49",
+      "location": "PF1",
+      "zone": "ห้องเคมี",
+      "order": 14
+    },
+    {
+      "id": "a50",
+      "location": "PF1",
+      "zone": "Retort CUP",
+      "order": 15
+    },
+    {
+      "id": "a51",
+      "location": "PF1",
+      "zone": "ห้องปล่อยกระป๋อง/ห้องปั้มลม",
+      "order": 16
+    },
+    {
+      "id": "a52",
+      "location": "PF1",
+      "zone": "ห้องเก็บสารเคมี",
+      "order": 17
+    },
+    {
+      "id": "a53",
+      "location": "PF1",
+      "zone": "ทางเข้าไลน์ PF1(ฝั่งโรงอาหาร1)",
+      "order": 18
+    },
+    {
+      "id": "a54",
+      "location": "PF2",
+      "zone": "ไลน์ CUP 6-7",
+      "order": 1
+    },
+    {
+      "id": "a55",
+      "location": "PF2",
+      "zone": "ไลน์ CAN",
+      "order": 2
+    },
+    {
+      "id": "a56",
+      "location": "PF2",
+      "zone": "Auto pouch",
+      "order": 3
+    },
+    {
+      "id": "a57",
+      "location": "PF2",
+      "zone": "Sachet",
+      "order": 4
+    },
+    {
+      "id": "a58",
+      "location": "PF2",
+      "zone": "Retort Store",
+      "order": 5
+    },
+    {
+      "id": "a59",
+      "location": "PF2",
+      "zone": "เตรียมกระป๋อง",
+      "order": 6
+    },
+    {
+      "id": "a60",
+      "location": "PF2",
+      "zone": "ห้องเคมี",
+      "order": 7
+    },
+    {
+      "id": "a61",
+      "location": "PF2",
+      "zone": "ห้องเก็บบู๊ท",
+      "order": 8
+    },
+    {
+      "id": "a62",
+      "location": "PF2",
+      "zone": "Office BI",
+      "order": 9
+    },
+    {
+      "id": "a63",
+      "location": "PF2",
+      "zone": "Office PF2",
+      "order": 10
+    },
+    {
+      "id": "a64",
+      "location": "PF2",
+      "zone": "Jerky",
+      "order": 11
+    },
+    {
+      "id": "a65",
+      "location": "PF2",
+      "zone": "อาคารสำนักงาน QA+LAB",
+      "order": 12
+    },
+    {
+      "id": "a66",
+      "location": "PF2",
+      "zone": "อาคารสำนักงาน ชั้น2",
+      "order": 13
+    },
+    {
+      "id": "a67",
+      "location": "PF2",
+      "zone": "อาคารสำนักงาน ชั้น3",
+      "order": 14
+    },
+    {
+      "id": "a68",
+      "location": "PF2",
+      "zone": "อาคารสำนักงาน ชั้น4",
+      "order": 15
+    },
+    {
+      "id": "a69",
+      "location": "PF2",
+      "zone": "ห้องล้างกะบะ",
+      "order": 16
+    },
+    {
+      "id": "a70",
+      "location": "PF2",
+      "zone": "freeze dry",
+      "order": 17
+    },
+    {
+      "id": "a71",
+      "location": "WH",
+      "zone": "ทางเข้า WH1 ออฟฟิคพัสดุ",
+      "order": 1
+    },
+    {
+      "id": "a72",
+      "location": "WH",
+      "zone": "WH1-BC",
+      "order": 2
+    },
+    {
+      "id": "a73",
+      "location": "WH",
+      "zone": "WH2-ปิดฉลาก",
+      "order": 3
+    },
+    {
+      "id": "a74",
+      "location": "WH",
+      "zone": "WH4-ส่งมอบสินค้า",
+      "order": 4
+    },
+    {
+      "id": "a75",
+      "location": "WH",
+      "zone": "WH4-ห้องน้ำด้านใน-ด้านนอก",
+      "order": 5
+    },
+    {
+      "id": "a76",
+      "location": "WH",
+      "zone": "WH2-ชั้นลอย",
+      "order": 6
+    },
+    {
+      "id": "a77",
+      "location": "WH",
+      "zone": "WH2-ห้องเก็บฉลากบ่อ1000",
+      "order": 7
+    },
+    {
+      "id": "a78",
+      "location": "WH",
+      "zone": "WH4-PKG-BC",
+      "order": 8
+    },
+    {
+      "id": "a79",
+      "location": "WH",
+      "zone": "WH5-PKG-BC",
+      "order": 9
+    },
+    {
+      "id": "a80",
+      "location": "WH",
+      "zone": "WH6-BC",
+      "order": 10
+    },
+    {
+      "id": "a81",
+      "location": "WH",
+      "zone": "WH7",
+      "order": 11
+    },
+    {
+      "id": "a82",
+      "location": "WH",
+      "zone": "WH8",
+      "order": 12
+    },
+    {
+      "id": "a83",
+      "location": "WH",
+      "zone": "สโตรกลาง",
+      "order": 13
+    },
+    {
+      "id": "a84",
+      "location": "WH",
+      "zone": "อาคารเก็บน้ำมันดีเซล",
+      "order": 14
+    }
+  ]
+};
